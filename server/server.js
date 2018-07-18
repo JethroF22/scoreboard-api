332b3d928940f1fa00d1a20d8a4fffeb7b25f140
Jethro W. Fredericks <fredericks22.jethro@gmail.com>
2018-07-18 14:44:47 +0200

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const env = process.env.NODE_ENV || 'development';
+
+if (env === 'development') {
+  require('dotenv').config();
+} else if (env === 'test') {
+  require('dotenv').config({ path: './.env.test' });
+}
+
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.listen(process.env.PORT, () => {
+  console.log(`Server is live on port ${process.env.PORT}`);
+});
+
+module.exports = app;
