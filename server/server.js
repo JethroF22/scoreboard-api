c7c3a99fb82c536a4eb742d29d773131511158b3
Jethro W. Fredericks <fredericks22.jethro@gmail.com>
2018-07-18 17:08:12 +0200

    Create POST /add route

--- server/server.js
+++ server/server.js
@@ -10,11 +10,30 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const _ = require('lodash');
 
 const mongoose = require('./db/mongoose');
+const Scoreboard = require('./models/scoreboard');
 
 const app = express();
 app.use(cors());
+app.use(bodyParser.json());
+
+app.post('/add', (req, res) => {
+  console.log(req.body);
+  const data = _.pick(req.body, ['name', 'score', 'time']);
+  console.log('data:', data);
+
+  Scoreboard.create(data)
+    .then(score => {
+      Scoreboard.find({}).then(scores => {
+        scores = scores.map(score => _.pick(score, ['name', 'score', 'time']));
+        res.send({ scores });
+      });
+    })
+    .catch(err => res.status(400).send());
+});
+
 app.listen(process.env.PORT, () => {
   console.log(`Server is live on port ${process.env.PORT}`);
 });
